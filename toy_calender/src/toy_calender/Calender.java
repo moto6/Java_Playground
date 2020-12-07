0e209e7cf3bf4568d01c332e817fcd55a3d976ef
DHKim <ultraraptor@naver.com>
2020-12-07 19:48:17 +0900

    prmpt,cal septe

--- toy_calender/src/toy_calender/Calender.java
+++ toy_calender/src/toy_calender/Calender.java
@@ -9,6 +9,41 @@ public class Calender {
 	private static final int[] MAX_DAYS= { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	private static final int[] LEAP_MAX_DAYS= { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 	
+	
+	
+	public static void main(String[] args) {
+		//test
+		//printCalender(2017,1);
+		
+		
+	}
+	
+	
+	public static void printCalender(int year ,int month) {
+		
+			System.out.printf("\n <<   %2d - %4d>>   \n", year, month);
+			System.out.println("SUN  MON  TUE  WED  THR  FRI  SAT");
+			System.out.println("---------------------------------");
+			System.out.println(" 1   2   3   4   5   6   7");
+			System.out.println(" 8   9  10  11  12  13  14");
+			System.out.println("15  16  17  18  19  20  21");
+			System.out.println("22  23  24  25  26  27  28");
+		
+	}
+	
+	public int getMaxDaysofMonth(int year, int month) {
+		//int calmonth[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		
+		if(isLeapyear(year)) {
+			return LEAP_MAX_DAYS[month];
+		}
+		else {
+			return MAX_DAYS[month];
+		}
+	}
+		
+	
+	
 	public boolean isLeapyear(int year) {
 		if(year%400 == 0) {
 			return true;
@@ -22,62 +57,6 @@ public class Calender {
 		}
 		else {
 			return false;
-		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Hello world!!");
-		
-		
-		for(int i=0 ; i<7 ; i++ ) {
-			System.out.print(yo_kr[i]+ "   ");
-		}
-		
-		System.out.println('\n'+LINE);
-		
-		for(int i=0 ; i<4 ; i++) {
-			for(int j=1 ; j<=7 ; j++) {
-				System.out.print(i*7+j+ "  ");
-			}
-			System.out.print('\n');
-		}
-
-		////////////////////////////////////////////////
-		
-	}
-	
-	
-	public void printCalender(int year ,int month) {
-		if(isLeapyear(year)) {
-			
-		}
-		else {
-			System.out.printf("\n    <<%4d - %3d>>   \n", year, month);
-			System.out.println(" 일   월  화  수  목  금   토");
-			System.out.println("--------------------------");
-			System.out.println(" 1   2   3   4   5   6   7");
-			System.out.println(" 8   9  10  11  12  13  14");
-			System.out.println("15  16  17  18  19  20  21");
-			System.out.println("22  23  24  25  26  27  28");
-		}
-	}
-	
-	public void getMaxDaysofMonth() {
-		//int calmonth[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		
-		int a = sc.nextInt();
-		if(1<= a && a<=12) {
-			System.out.printf("%d월은 %d일까지 있습니다", a, calmonth[a]);
-			//cal.printSampleCal(2020,)
-			cal.printCal(2020,a);
-			
-		}
-		else if(a == 0 || a==-1) {
-			System.out.printf("종료키 입력으로 프로그램 정지");
-			break;
-		}
-		else {
-			//이상한거 입력됨
 		}
 	}
 	
--- toy_calender/src/toy_calender/Prompt.java
+++ toy_calender/src/toy_calender/Prompt.java
@@ -6,24 +6,49 @@ public class Prompt {
 	private final static String PROMPT = "Calender> ";
 	
 	public void runPrompt() {
-		
 		Scanner sc = new Scanner(System.in);
-		Calender cal = new Calender();
+		Calender mycal = new Calender();
+		
+		int year = 2000;
+		int month = 1;
+		int weekday = 0;
+
+		System.out.println("달을 입력하세요");
+		System.out.print("Year> ");
+		year = sc.nextInt();
+
+		System.out.println("월을 입력하세요");
+		System.out.print("Month> ");
+		month = sc.nextInt();
+
+		System.out.println("SUN/MON/TUE/WED/TUR/FRI/SAT");
+		System.out.println("요일을 입력하세요");
+		System.out.print("WekD> ");
+		String sweek = sc.nextLine();
+		weekday = parseDay(sweek);
 		
 		
-		while(true) {
-			System.out.println("달을 입력하세요");
-			System.out.print(PROMPT);
-			
-		}
-		System.out.println("ByeBye!");
-		sc.close();
-		
+		mycal.printCalender(year, month);
 	}
 	
 	public static void main(String[] args) {
 		Prompt p = new Prompt();
 		p.runPrompt();
 			
+	}
+	
+	
+	public static int parseDay(String week) {
+		//-1 : ERROR, wrong day
+		//0~6 ; sun~mon
+		
+		
+		final String[] YOIL = {"SUN","MON","TUE","WED","TUR","FRI","SAT"};
+		for(int i=0 ; i<7 ; i++) {
+			if(YOIL[i].equals(week)) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
