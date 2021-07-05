989add754623439fce00178d49fe1e27f8774bad
DHKim <ultraraptor@naver.com>
2021-07-06 01:29:07 +0900

    FILE IO

---
+++ ExampleCode/src/main/java/chp11_enum/ex1/Member.java
@@ -0,0 +1,31 @@
+package chp11_enum.ex1;
+
+public class Member implements Serializable {
+    private final String name;
+    private final String email;
+    private final int age;
+    Member member = new Member("김배민", "deliverykim@baemin.com", 25);
+    byte[] serializedMember;
+}
+
+    public Member(String name, String email, int age) {
+        this.name = name;
+        this.email = email;
+        this.age = age;
+    }
+
+    // Getter 생략
+    @Override
+    public String toString() {
+        return String.format("Member", name, email, age);
+    }
+    try(ByteArrayOutputStream baos=new ByteArrayOutputStream()){
+
+            try
+            }(ObjectOutputStream oos=new ObjectOutputStream(baos)){
+            oos
+            }.writeObject(member); // serializedMember -> 직렬화된 member 객체 serializedMember = baos.toByteArray(); } } // 바이트 배열로 생성된 직렬화 데이터를 base64로 변환 System.out.println(Base64.getEncoder().encodeToString(serializedMember)); }
+
+
+
+
